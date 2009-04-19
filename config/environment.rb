156a1642cce05a6c2d2552abdb3a124de13dc7e9
David Cato <git@crunchyfrog.net>
2009-04-18 17:22:58 -0700

    Add missing :lib option for mislav-will_paginate gem

--- config/environment.rb
+++ config/environment.rb
@@ -38,7 +38,7 @@ Rails::Initializer.run do |config|
   config.gem 'ruby-openid', :version => '>2.1', :lib => 'openid'
   config.gem 'rubyzip', :version => '>=0.9.1', :lib => 'zip/zip'
   config.gem 'tzinfo', :version => '>=0.3.12'
-  config.gem 'mislav-will_paginate', :version => '>=2.3.8',
+  config.gem 'mislav-will_paginate', :version => '>=2.3.8', :lib => 'will_paginate',
                :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
