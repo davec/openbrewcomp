86d31271613227956e029f6253e7adba84cbc37c
David Cato <git@crunchyfrog.net>
2009-04-27 01:44:58 -0700

    Handle style description URLs with a leading /

--- app/helpers/styles_helper.rb
+++ app/helpers/styles_helper.rb
@@ -6,7 +6,7 @@ module StylesHelper
     if description_url =~ %r{\A(ftp|https?)://}
       external_link = description_url
     else
-      parts = description_url.split('/')
+      parts = description_url.split('/').delete_if{|a| a.blank?}
       # If the URL is terminated by a / no action should be specified
       action = parts.pop unless description_url[-1,1] == '/'
       # NOTE: In the test environment, a leading / is required on the
@@ -19,15 +19,13 @@ module StylesHelper
   end
 
   def special_styles
-    Style.find(:all,
-               :conditions => 'bjcp_category > 28',
-               :order => 'bjcp_category')
+    Style.all(:conditions => 'bjcp_category > 28',
+              :order => 'bjcp_category')
   end
 
   def styles_with_required_styleinfo
-    Style.find(:all,
-               :conditions => [ 'styleinfo = ?', StyleInfo::REQUIRED_KEY ],
-               :order => 'bjcp_category, bjcp_subcategory')
+    Style.all(:conditions => [ 'styleinfo = ?', StyleInfo::REQUIRED_KEY ],
+              :order => 'bjcp_category, bjcp_subcategory')
   end
 
 end
