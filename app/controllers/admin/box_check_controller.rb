9b346e6ada7406a05e2953ccb6a19e299337b290
David Cato <git@crunchyfrog.net>
2009-04-28 15:35:32 -0700

    The box check report is now only generated as PDF

--- app/controllers/admin/box_check_controller.rb
+++ app/controllers/admin/box_check_controller.rb
@@ -3,21 +3,15 @@
 class Admin::BoxCheckController < AdministrationController
 
   def index
-    @categories = Category.find(:all,
-                                :include => [ :awards, :styles ],
-                                :conditions => [ 'categories.is_public = ?', true ],
-                                :order => 'categories.position')
-    #@awards = Award.find(:all,
-    #                     :include => [ :styles ],
-    #                     :order => 'category_id, position')
-
     respond_to do |format|
-      format.html
-      format.pdf {
+      format.pdf do
+        @categories = Category.all(:include => [ :awards, :styles ],
+                                   :conditions => [ 'categories.is_public = ?', true ],
+                                   :order => 'categories.position')
         options_for_rtex = { :preprocess => true, :filename => "box_check.pdf" }
         options_for_rtex.merge({ :debug => true, :shell_redirect => "> #{File.expand_path(RAILS_ROOT)}/tmp/box_check.rtex.log 2>&1" }) if ENV['RAILS_ENV'] == 'development'
         render options_for_rtex.merge(:layout => false)
-      }
+      end
     end
   end
 
--- test/functional/admin/box_check_controller_test.rb
+++ test/functional/admin/box_check_controller_test.rb
@@ -6,23 +6,10 @@ class Admin::BoxCheckControllerTest < ActionController::TestCase
 
   def setup
     login_as(:admin)
-    @competition_name = CompetitionData.instance.name
   end
 
-  def test_index
+  def test_should_generate_pdf
     get :index
-    assert_response :success
-
-    # Verify the page content
-    assert_select 'html > head > title', "#{@competition_name} Box Check"
-    assert_select 'div#content' do
-      assert_select 'h1', "#{@competition_name} Box Check"
-      assert_select 'table', :count => 1
-    end
-  end
-
-  def test_print
-    get :index, :format => 'pdf'
     assert_response :success
     assert_pdf_response
   end
