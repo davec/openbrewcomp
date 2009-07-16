c42128a8a65132049223a4fd9d6e931e04fa4786
David Cato <git@crunchyfrog.net>
2009-07-15 22:05:37 -0700

    Note issue with relative_url_root and passenger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,10 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+# HACK: If using Phusion Passenger 2.2.2 or earlier and deploying in a subdirectory, set relative_url_root.
+# See http://code.google.com/p/phusion-passenger/issues/detail?id=169 for details.
+# config.action_controller.relative_url_root = '/path_prefix'
+
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
 
