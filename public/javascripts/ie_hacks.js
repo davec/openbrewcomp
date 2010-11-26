aa39ced728b9c9b737e2efb5b313b536386e0e08
David Cato <git@crunchyfrog.net>
2010-11-26 12:53:28 -0800

    Code cleanup of IE-specific JS

--- public/javascripts/ie_hacks.js
+++ public/javascripts/ie_hacks.js
@@ -1,44 +1,24 @@
-/* [TODO] Is all this tag name checking really required? It shouldn't be, but this is IE we're dealing with here. */
-navigationStartList = function()
-{
-  if (document.all && document.getElementById)
-  {
-    body_width = document.body.clientWidth;
-    $('navigation').immediateDescendants().each(function(e) {
-      if (e.tagName == 'LI')
-      {
-        if (e.previous() == undefined)
-        {
-          e.addClassName('firstlink');
+navigationStartList = function() {
+  if (document.all && document.getElementById) {
+    var body_width = document.body.clientWidth;
+
+    $('navigation').childElements().each(function(e,i) {
+      if (i === 0) e.addClassName('firstlink');
+
+      e.onmouseover = function() { e.addClassName('over'); };
+      e.onmouseout = function() { e.removeClassName('over'); };
+      e.childElements().each(function(e) {
+        if (e.tagName == 'UL') {
+          e.style.width = body_width;
+          e.childElements().first().addClassName('firstlink');
         }
-        e.onmouseover = function() { e.addClassName('over'); }
-        e.onmouseout = function() { e.removeClassName('over'); }
-        e.immediateDescendants().each(function(e) {
-          if (e.tagName == 'UL')
-          {
-            e.style.width = body_width;
-            e.immediateDescendants().each(function(e) {
-              if (e.tagName == 'LI')
-              {
-                if (e.previous() == undefined)
-                {
-                  e.addClassName('firstlink');
-                }
-              }
-            })
-          }
-        });
-      }
+      });
     });
 
-    if ($('reports-menu'))
-    {
-      $('reports-menu').immediateDescendants().each(function(e) {
-        if (e.tagName == 'LI')
-        {
-          e.onmouseover = function() { this.className += " over"; }
-          e.onmouseout = function() { this.className = this.className.replace(" over", ""); }
-        }
+    if ($('reports-menu')) {
+      $('reports-menu').childElements().each(function(e) {
+        e.onmouseover = function() { e.addClassName('over'); };
+        e.onmouseout = function() { e.removeClassName('over'); };
       });
     }
   }
